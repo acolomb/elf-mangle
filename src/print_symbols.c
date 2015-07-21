2fb487e0f371f7cd0f0dd84967eabd4af3b5bd5c
André Colomb <src@andre.colomb.de>
2015-07-21 17:59:37 +0200

    Fix missing internationalization calls.
    Try to look up a translation for the known fields' descriptions when
    printing symbols.  In the custom_known_fields example code, these are
    explicitly marked as translatable, but gettext() was not called at
    runtime.
    
    Add a missing translation marker for the byte count too.

--- src/print_symbols.c
+++ src/print_symbols.c
@@ -59,8 +59,8 @@ print_symbol_description_iterator(
     if (*conf & showAddress) printf("[%04zx]\t", symbol->offset);
     if ((*conf & showSymbol) ||
 	(! symbol->field->print_func)) printf("%s:", symbol->field->symbol);
-    else printf("%s:", symbol->field->description);
-    if (*conf & showByteSize) printf(" %zu bytes", symbol->size);
+    else printf("%s:", _(symbol->field->description));
+    if (*conf & showByteSize) printf(_(" %zu bytes"), symbol->size);
     if (DEBUG) printf(" %p", symbol->blob_address);
 
     return NULL;	//continue iterating
